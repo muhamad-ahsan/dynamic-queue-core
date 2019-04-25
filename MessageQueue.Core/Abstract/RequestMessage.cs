256e3998486e5e4db59bad546f73ee5c7d390180
Muhammad Ahsan <Muhamad_Ahsan@LIVE.COM>
2019-04-25 23:02:50 +0400

    Core components added.

---
+++ MessageQueue.Core/Abstract/RequestMessage.cs
@@ -0,0 +1,23 @@
+﻿namespace MessageQueue.Core.Abstract
+{
+    /// <summary>
+    /// Base class for different types of request message.
+    /// </summary>
+    public abstract class RequestMessage<TRequest, TResponse>
+    {
+        #region Public Data Members
+        /// <summary>
+        /// Request data.
+        /// </summary>
+        public TRequest RequestData { get; protected set; }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Responses back to the client.
+        /// </summary>
+        /// <param name="response">The response object</param>
+        public abstract void Response(TResponse response);
+        #endregion
+    }
+}
